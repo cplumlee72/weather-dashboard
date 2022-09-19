60c7c29bccfd57c1fbbf08c7f50d006963ae3ff6
Caleb <bambini6@gmail.com>
2022-09-18 23:51:19 -0500

    Added weather API and a function to get weather from the dataset and render onto page based on city that user selected.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,29 @@
-var weatherurl = 'api.openweathermap.org/data/2.5/forecast?q={city name}&units=standard&appid=cb4022af9ee9d0ebd691e41110e4c85b'
+
 var userSearch = $(".userInput")[0]
-var currentCityCond = document.querySelectorAll(".curCty")[0]
+var currentCityCond = document.querySelectorAll(".curCty")[0].children
  
-console.log(currentCityCond.children)
+console.log(currentCityCond)
+
+function getWeather() {    
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userSearch.value +'&units=imperial&appid=cb4022af9ee9d0ebd691e41110e4c85b')
+        .then(function(resp) { return resp.json() }) // Convert data to json
+        .then(function(data) {
+            (currentCityCond[1].children[0]).innerHTML = "Temp: " + data.main.temp+ "&deg; F";
+            (currentCityCond[1].children[1]).innerHTML = "Wind: " + data.wind.speed+ " MPH";
+            (currentCityCond[1].children[2]).innerHTML = "Humidity: " + data.main.humidity+ " %"; 
+            console.log(data);
+            
+  })
+  
+}
 
 function searchCity(event) {
     console.log(event);
-    currentCityCond.children[0].textContent = userSearch.value
-    $(".cities").append('<button class= "list-group-item rounded-3 py-3">'+userSearch.value+'</button>') 
+    currentCityCond[0].textContent = userSearch.value
+    
+    $(".cities").append('<button class= "list-group-item rounded-3 py-3">'+userSearch.value+ '</button>') 
     console.log(userSearch.value)
+    getWeather();
 };
 
 $(".btn").on("click", searchCity);
