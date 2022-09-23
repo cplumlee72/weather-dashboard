f39722349fda5b5342f97b0202448c09e1f9e434
Caleb <bambini6@gmail.com>
2022-09-23 01:53:16 -0500

    Added a ton of javascript to fetch forecast data and render it.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,53 @@ function getWeather() {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userSearch.value +'&units=imperial&appid=cb4022af9ee9d0ebd691e41110e4c85b')
         .then(function(resp) { return resp.json() }) // Convert data to json
         .then(function(data) {
+            var coordinates = Object.values(data.coord);
+            
+            console.log(data)
+            console.log(coordinates);
+            coordinates.forEach(value => {
+                console.log(value)
+                window.localStorage.setItem(userSearch.value, JSON.stringify(coordinates))
+            });
+            console.log(JSON.parse(localStorage.getItem(userSearch.value))); 
             (currentCityCond[1].children[0]).innerHTML = "Temp: " + data.main.temp+ "&deg; F";
             (currentCityCond[1].children[1]).innerHTML = "Wind: " + data.wind.speed+ " MPH";
-            (currentCityCond[1].children[2]).innerHTML = "Humidity: " + data.main.humidity+ " %"; 
-            console.log(data);
-            
-  })
-  
+            (currentCityCond[1].children[2]).innerHTML = "Humidity: " + data.main.humidity+ " %";
+            getFiveDay();                       
+  })  
 }
+
+function getFiveDay() {
+    var coordinates = JSON.parse(localStorage.getItem(userSearch.value))
+    console.log(coordinates[0]);
+    console.log(coordinates[1]);
+    // fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + userSearch.value + '&cnt=5&units=imperial&exclude=hourly&appid=cb4022af9ee9d0ebd691e41110e4c85b')
+    fetch('https://api.openweathermap.org/data/3.0/onecall?lat='  + coordinates[1] + '&lon=' + coordinates[0] + '&exclude=current,hourly,minutely,alerts&units=imperial&appid=cb4022af9ee9d0ebd691e41110e4c85b')
+
+    .then(function(resp) { return resp.json() }) // Convert data to json
+        .then(function(data) {
+            var fcdate = document.querySelectorAll(".fcdate");
+            var fccond = document.querySelectorAll(".fccond");
+            var fctemp = document.querySelectorAll(".fctemp");
+            var fcwind = document.querySelectorAll(".fcwind");
+            var fchum = document.querySelectorAll(".fchum");     
+            console.log(data.daily)
+            data.daily.forEach((value, index) => {
+                if (index < 5) {
+                    var date = new Date(value.dt * 1000).toLocaleDateString("en-US");
+                    // var weathericon = value.weather[0]
+                    console.log(value.weather[0].icon);
+                    console.log(date);
+                    console.log(document.querySelectorAll(".fccond"))
+                    fcdate[index].textContent = date
+                    fccond[index].innerHTML = '<img src="http://openweathermap.org/img/wn/' + value.weather[0].icon + '@2x.png"></img>' 
+                }
+            })
+                             
+  })
+}
+
+
 
 function searchCity(event) {
     console.log(event);
@@ -24,6 +63,7 @@ function searchCity(event) {
     $(".cities").append('<button class= "list-group-item rounded-3 py-3">'+userSearch.value+ '</button>') 
     console.log(userSearch.value)
     getWeather();
+    
 };
 
 $(".btn").on("click", searchCity);
