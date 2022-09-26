d47c87a8169a463fe5a24b03800138736f5a390d
Caleb <bambini6@gmail.com>
2022-09-25 23:24:07 -0500

    Finalized functionality of weather dashboard, now just need to polish. Added a handler for clicking the city name buttons, and added functionality for adding search terms to local storage, and generating the buttons from local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 
-var userSearch = $(".userInput")[0]
-var currentCityCond = document.querySelectorAll(".curCty")[0].children
- 
-console.log(currentCityCond)
+var userSearch = $(".userInput")[0];
+var currentCityCond = document.querySelectorAll(".curCty")[0].children;
+
+
+
+
+
 
 function getWeather() {    
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userSearch.value +'&units=imperial&appid=cb4022af9ee9d0ebd691e41110e4c85b')
@@ -10,24 +13,20 @@ function getWeather() {
         .then(function(data) {
             var coordinates = Object.values(data.coord);
             
-            console.log(data)
-            console.log(coordinates);
+            console.log(data);
             coordinates.forEach(value => {
-                console.log(value)
-                window.localStorage.setItem(userSearch.value, JSON.stringify(coordinates))
-            });
-            console.log(JSON.parse(localStorage.getItem(userSearch.value))); 
-            (currentCityCond[1].children[0]).innerHTML = "Temp: " + data.main.temp+ "&deg; F";
-            (currentCityCond[1].children[1]).innerHTML = "Wind: " + data.wind.speed+ " MPH";
-            (currentCityCond[1].children[2]).innerHTML = "Humidity: " + data.main.humidity+ " %";
+                window.localStorage.setItem(userSearch.value + 'coords', JSON.stringify(coordinates));
+            });     
+            (currentCityCond[1].children[0]).innerHTML = '<img src="http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png"></img>';;        
+            (currentCityCond[1].children[1]).innerHTML = "Temp: " + data.main.temp+ "&deg; F";
+            (currentCityCond[1].children[2]).innerHTML = "Wind: " + data.wind.speed+ " MPH";
+            (currentCityCond[1].children[3]).innerHTML = "Humidity: " + data.main.humidity+ " %";
             getFiveDay();                       
   })  
 }
 
 function getFiveDay() {
-    var coordinates = JSON.parse(localStorage.getItem(userSearch.value))
-    console.log(coordinates[0]);
-    console.log(coordinates[1]);
+    var coordinates = JSON.parse(localStorage.getItem(userSearch.value + 'coords'));
     fetch('https://api.openweathermap.org/data/3.0/onecall?lat='  + coordinates[1] + '&lon=' + coordinates[0] + '&exclude=current,hourly,minutely,alerts&units=imperial&appid=cb4022af9ee9d0ebd691e41110e4c85b')
 
     .then(function(resp) { return resp.json() }) // Convert data to json
@@ -37,15 +36,11 @@ function getFiveDay() {
             var fctemp = document.querySelectorAll(".fctemp");
             var fcwind = document.querySelectorAll(".fcwind");
             var fchum = document.querySelectorAll(".fchum");     
-            console.log(data.daily)
             data.daily.forEach((value, index) => {
                 if (index < 5) {
                     var date = new Date(value.dt * 1000).toLocaleDateString("en-US");
-                    console.log(value.temp.max);
-                    console.log(date);
-                    console.log(document.querySelectorAll(".fccond"))
-                    fcdate[index].textContent = date
-                    fccond[index].innerHTML = '<img src="http://openweathermap.org/img/wn/' + value.weather[0].icon + '@2x.png"></img>' 
+                    fcdate[index].textContent = date;
+                    fccond[index].innerHTML = '<img src="http://openweathermap.org/img/wn/' + value.weather[0].icon + '@2x.png"></img>';
                     fctemp[index].innerHTML = "Temp: " + value.temp.max + " &deg; F";
                     fcwind[index].innerHTML = "Wind: " + value.wind_speed + " MPH";
                     fchum[index].innerHTML = "Humidity: " + value.humidity + "%"
@@ -58,14 +53,52 @@ function getFiveDay() {
 
 
 function searchCity(event) {
-    console.log(event);
-    currentCityCond[0].textContent = userSearch.value    
-    $(".cities").append('<button class= "list-group-item rounded-3 py-3">'+userSearch.value+ '</button>') 
-    console.log(userSearch.value)
+    if (cityArr.indexOf(userSearch.value.toUpperCase()) === -1){
+    cityArr.push(userSearch.value.toUpperCase())
+    window.localStorage.setItem("newCity", JSON.stringify(cityArr));
+    $(".cities").append('<button class= "list-group-item rounded-3 py-3">'+userSearch.value.toUpperCase()+ '</button>') 
+    } else { console.log("ERROR")}
+    
+    currentCityCond[0].textContent = userSearch.value.toUpperCase();
     getWeather();
     
 };
 
+
+var cityArr = JSON.parse(window.localStorage.getItem("newCity"))
+
+function generateButtons() {
+    console.log(cityArr);
+    cityArr.forEach(Element => {
+        console.log(Element);
+        if (Element) {        
+      $(".cities").append('<button class="list-group-item rounded-3 py-3 ctybtn">'+Element+ '</button>')
+        }        
+    })  
+};
+
+function setStorage(){
+    if (!window.localStorage.getItem("newCity")) {
+    var startCities = ["DALLAS","DENVER"];
+    window.localStorage.setItem("newCity", JSON.stringify(startCities)); 
+    } ;
+    
+    console.log(cityArr.indexOf(userSearch.value.toUpperCase))
+    return;
+    
+           
+};
+
+function cityButtonHandler(event){
+    currentCityCond[0].textContent = event.target.textContent;
+    userSearch.value = event.target.textContent
+    getWeather();
+}
+
+
+window.addEventListener("load", setStorage);
+generateButtons();
+
+
 $(".btn").on("click", searchCity);
-
-
+$(".ctybtn").on("click", cityButtonHandler);
